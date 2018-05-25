75a3ccd398819201e667efbec8c86237511286de
Rakesh <rakesh@github.com>
2018-05-25 23:40:24 +0530

    Added new java file

---
+++ JenkinsDemo.java
@@ -0,0 +1,10 @@
+public class JenkinsDemo {
+    public static void main(String args[]){
+
+        System.out.println("First Jenkins Job");
+
+	for(int i=0;i<=10;i++){	
+        System.out.println("I'm dancing" +i);
+	}
+    }
+}
